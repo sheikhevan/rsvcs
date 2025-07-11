15811ec1f750398e220733c597bb46bdeff49ad3
Evan Alvarez <evanfalvarez@tutamail.com>
2025-07-10 22:43:41 -0500

    imma work on it later trust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long, default_value = "~/.config/rsbackup/config")]
+    config: String,
+}
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!("Config file located at -> {}", args.config);
 }
