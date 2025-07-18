d3f2014bf0251ec0dc10a4512bc1297b6b6544cd
Evan Alvarez <evanfalvarez@tutamail.com>
2025-07-18 14:39:21 -0500

    added all the crates

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use clap::Parser;
+mod parse_config;
+
+#[derive(Parser, Debug)]
+#[command(
+    name = "rsvcs",
+    version = "0.0.1",
+    about = "Simple version control system written in Rust"
+)]
+
+struct Cli {
+    #[arg(short, long)]
+    verbose: bool,
+
+    #[arg(short, long)]
+    add: Option<String>,
+
+    #[arg(short, long)]
+    commit: Option<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
 }
