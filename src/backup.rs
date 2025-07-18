00ce989f3e368821be84e0a1bd6521df0aa4d99c
Evan Alvarez <evanfalvarez@tutamail.com>
2025-07-18 02:22:26 -0500

    fixed by trimming the '/' off of the end of exclusions

--- src/backup.rs
+++ src/backup.rs
@@ -18,13 +18,17 @@ pub fn backup(
     sources: &Vec<String>,
     exclusions: &Vec<String>,
 ) -> Result<(), Box<dyn std::error::Error>> {
+    let trimmed_exclusions: Vec<String> = exclusions
+        .iter()
+        .map(|s| s.strip_suffix('/').unwrap_or(&s).to_string())
+        .collect();
     for destination in destinations {
         for source in sources {
             let source_path = PathBuf::from(source);
             let dest_path = PathBuf::from(destination);
             for entry in WalkDir::new(&source_path)
                 .into_iter()
-                .filter_entry(|e| !is_excluded(e, exclusions))
+                .filter_entry(|e| !is_excluded(e, &trimmed_exclusions))
             {
                 let entry = entry?;
 
@@ -35,7 +39,7 @@ pub fn backup(
                 }
 
                 // this creates the directories
-                if entry.file_type().is_dir() {
+                if entry.file_type().is_dir() && !is_excluded(&entry, &trimmed_exclusions) {
                     if let Err(e) = fs::create_dir(to) {
                         match e.kind() {
                             io::ErrorKind::AlreadyExists => {}
@@ -47,7 +51,7 @@ pub fn backup(
                 else if entry.file_type().is_file() {
                     fs::copy(from, to)?;
                 } else {
-                    eprintln!("ignored {}", from.display());
+                    eprintln!("Ignored {}", from.display());
                 }
             }
         }
