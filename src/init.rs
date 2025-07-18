3a83febbafc731aabd9e968c9f5e8cdea10202a6
Evan Alvarez <evanfalvarez@tutamail.com>
2025-07-18 15:36:29 -0500

    stuff

--- src/init.rs
+++ src/init.rs
@@ -1,8 +1,10 @@
-use std::fs;
+use std::{fs, io::Write};
 
 pub fn init() -> std::io::Result<()> {
     fs::create_dir_all(".rsvcs/commits")?;
     fs::create_dir_all(".rsvcs/logs")?;
     fs::File::create(".rsvcs/config.toml")?;
+    let mut description = fs::File::create(".rsvcs/description")?;
+    description.write_all(b"This repository is unnamed! Edit this file to name the repository.")?;
     Ok(())
 }
